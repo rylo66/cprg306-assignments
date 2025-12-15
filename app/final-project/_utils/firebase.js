ae744070ee4db55ed31b731157ddb7ad26351ed5
Riley Yonda <yondariley@gmail.com>
2025-12-14 20:30:56 -0700

    Remove Firebase config variables from firebase.js
    Removed Firebase configuration environment variables from the file.

--- app/final-project/_utils/firebase.js
+++ app/final-project/_utils/firebase.js
@@ -16,10 +16,3 @@ const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-NEXT_PUBLIC_FIREBASE_API_KEY="AIzaSyAddYgr-qbkPZB2Oc8J7Ji15TKA0NCKZpQ"
-NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="week-9-bdf3e.firebaseapp.com"
-NEXT_PUBLIC_FIREBASE_PROJECT_ID="week-9-bdf3e"
-NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="week-9-bdf3e.firebasestorage.app"
-NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="985250300610"
-NEXT_PUBLIC_FIREBASE_APP_ID="1:985250300610:web:09a70e014a220822ffb15b"
